f078b1655bbf4a86688bb96e8c0130eff5ab5b83
Dzung Nguyen <dungnq16@fpt.com>
2026-01-08 17:28:11 +0700

    Initial Commit

---
+++ ios-clean-architecture-swiftui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios-clean-architecture-swiftui
+//
+//  Created by Nguyen Dzung on 8/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios-clean-architecture-swiftui/ios_clean_architecture_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_clean_architecture_swiftuiApp.swift
+//  ios-clean-architecture-swiftui
+//
+//  Created by Nguyen Dzung on 8/1/26.
+//
+
+import SwiftUI
+
+@main
+struct ios_clean_architecture_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
